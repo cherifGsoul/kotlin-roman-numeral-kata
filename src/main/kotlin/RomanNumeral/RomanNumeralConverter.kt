2c5fa0ed32a73370cd0b883d5163465d2ce040ca
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-09-24 15:15:37 +0100

    Configure Romans

--- src/main/kotlin/RomanNumeral/RomanNumeralConverter.kt
+++ src/main/kotlin/RomanNumeral/RomanNumeralConverter.kt
@@ -4,7 +4,20 @@
 package RomanNumeral
 
 object RomanNumeralConverter {
-    private val romans = listOf(1000 to "M", 500 to "D", 100 to "C", 10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I").toMap()
+    private val romans = listOf(
+            1000 to "M",
+            900  to "CM",
+            500  to "D",
+            400  to "CD",
+            100  to "C",
+            90   to "XC",
+            50   to "L",
+            40   to "XL",
+            10   to "X",
+            9    to "IX",
+            5    to "V",
+            4    to "IV",
+            1    to "I").toMap()
 
     fun fromArabic(arabic: Int): String {
         var roman = ""
